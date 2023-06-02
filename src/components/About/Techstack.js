3309bc1d57fc8b026dac0722ef5363a0f8047eeb
virgel1995 <kazouya44@gmail.com>
2023-06-02 20:07:27 +0300

    ts

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,14 +1,11 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { CgCPlusPlus } from "react-icons/cg";
 import {
   DiJavascript1,
   DiReact,
   DiNodejs,
   DiMongodb,
-  DiPython,
   DiGit,
-  DiJava,
 } from "react-icons/di";
 import {
   SiRedis,
@@ -17,19 +14,17 @@ import {
   SiSolidity,
   SiPostgresql,
 } from "react-icons/si";
-import { TbBrandGolang } from "react-icons/tb";
+import { TbBrandPhp } from "react-icons/tb";
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <CgCPlusPlus />
-      </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <TbBrandGolang />
+        <TbBrandPhp />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiNodejs />
@@ -57,12 +52,6 @@ function Techstack() {
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiPostgresql />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiPython />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJava />
       </Col>
     </Row>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,7 +18,7 @@ function Footer() {
           <h3>Designed and Developed by Virus24</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} SB</h3>
+          <h3>Copyright © {year} Virus24</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
